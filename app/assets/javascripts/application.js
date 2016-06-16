320e545a622e26f80e4475e5b960085a59dbe4fc
Jejkej <jakub.kopys@tlen.pl>
2016-06-16 22:09:51 +0200

    lightbox for post images

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,31 @@
 //= require private_pub
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function() {
+    $(".post_thumb").click(function(e){
+        var image_href = $(this).attr("href");
+        $(".box").html($('<img>',{class:'added',src:image_href}));
+        var img = document.getElementsByClassName('added');
+        var width = img.naturalWidth;
+        var height = img.naturalHeight;
+        $(".backdrop").animate({'opacity':'0.5'}, 300, 'linear');
+        $(".box").animate({'opacity':'1.0'}, 300, 'linear');
+        $(".backdrop, .box").css("display", "block");
+        $(".backdrop").css("height", $(document).height());
+        $(".added").css("top", $(window).scrollTop()+60);
+        e.preventDefault();
+    });
+
+    $('.backdrop').click(function(){
+        close_box();
+    });
+
+});
+
+function close_box()
+{
+    $('.backdrop, .box, .added').animate({'opacity':'0'}, 300, 'linear', function(){
+        $('.backdrop, .box, .added').css('display', 'none');
+    });
+}
